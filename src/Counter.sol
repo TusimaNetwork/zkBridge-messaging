6ca5d328d295af7432a1180299d94a4279e48dd1
iwan <iwanwang2312@gmail.com>
2023-10-18 21:43:01 +0800

    solc version

--- src/Counter.sol
+++ src/Counter.sol
@@ -1,6 +1,8 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
+import {ERC1967Proxy} from "@openzeppelin/contracts/proxy/ERC1967/ERC1967Proxy.sol";
+
 contract Counter {
     uint256 public number;
 
