12bdc9c06494f8ff85b6a00567de34b6dbb2f9fb
iwan <iwanwang2312@gmail.com>
2023-10-19 16:02:53 +0800

    remapping

--- src/Counter.sol
+++ src/Counter.sol
@@ -2,6 +2,10 @@
 pragma solidity ^0.8.13;
 
 import {ERC1967Proxy} from "@openzeppelin/contracts/proxy/ERC1967/ERC1967Proxy.sol";
+import {ReentrancyGuard} from "@openzeppelin/contracts/security/ReentrancyGuard.sol";
+import {ReentrancyGuardUpgradeable} from "@openzeppelin/contracts-upgradeable/security/ReentrancyGuardUpgradeable.sol";
+// import { ILightClientGetter as ILightClient } from "zkBridge-lightClient/contracts/src/interfaces/ILightClient.sol";
+// import { ILightClientGetter as ILightClient } from "./interfaces/ILightClient.sol";
 
 contract Counter {
     uint256 public number;
