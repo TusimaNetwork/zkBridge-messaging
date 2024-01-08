89267aeb59b85c387dd56dfcebebfbfef803a13a
CornersOfTheCity <18892207018@163.com>
2024-01-08 18:33:05 +0800

    feat:update messaging script

--- script/DeployMessaging.s.sol
+++ script/DeployMessaging.s.sol
@@ -4,14 +4,14 @@ pragma solidity ^0.8.13;
 import "forge-std/Script.sol";
 import {Messaging} from "../src/Messaging.sol";
 
-contract DeployMessing is Script {
+contract DeployMessaging is Script {
     Messaging public messaging;
 
     function run() external {
         vm.startBroadcast(vm.envUint("PRIVATE_KEY"));
         messaging = new Messaging(vm.envBool("sendingEnabled"));
 
-        messaging.setRelayer(msg.sender, true);
+        messaging.setRelayer(vm.envAddress("broadcasters"), true);
 
         vm.stopBroadcast();
     }
